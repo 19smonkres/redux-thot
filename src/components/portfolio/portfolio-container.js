944eaadb96dfa3bc40a5b2a0183d095bf613e3ff
Savanah Monkres <sfmonkres@gmail.com>
2019-07-10 11:15:29 -0600

    Second commit: Class vs FCR stuff

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import PortfolioItem from "./porfolio-item";
+
+export default class PortfolioContainer extends Component {
+    // State
+    // Lifecycle hooks
+    render(){
+        <div>
+            <h2>Portfolio items go here...</h2>
+
+            <PortfolioItem />
+        </div>
+    }
+}
