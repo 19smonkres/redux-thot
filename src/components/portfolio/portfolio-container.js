166eedfc4af46fb0cac93a242c866919efe48b79
Savanah Monkres <sfmonkres@gmail.com>
2019-07-11 08:28:28 -0600

    Third, props yeet

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
 
-import PortfolioItem from "./porfolio-item";
+import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-    // State
-    // Lifecycle hooks
-    render(){
-        <div>
-            <h2>Portfolio items go here...</h2>
+    constructor() {
+        super();
+      
+        console.log("Portfolio container has rendered");
+    }
+    portfolioItems() {
+        const data = ["Quip", "eventbrite", "Ministry Safe", "SwingAway"];
 
-            <PortfolioItem />
-        </div>
+        return data.map(item => {
+            return <PortfolioItem title={item} url={"google.com"} />
+        })
+    }
+    
+    render(){
+        return (
+            <div>
+                <h2>Portfolio items go here...</h2>
+
+                {this.portfolioItems()}
+            </div>
+        );
     }
 }
